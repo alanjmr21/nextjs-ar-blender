45e6a4bc25476beb98cf5b8dffdd718bde42b047
Alan Muñoz <alanjmr206@gmail.com>
2024-09-03 15:22:17 -0700

    add silverbullet

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-200">
-      <a href="intent://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;">Avocado</a>
+      <a href="intent://arvr.google.com/scene-viewer/1.0?file=silverbullet.glb;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;">Avocado</a>
     </div>
   );
 }
