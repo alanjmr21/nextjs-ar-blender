4994a4acf28d1cf10e2bfbb103a3ca93b24dfbf7
Alan Muñoz <alanjmr206@gmail.com>
2024-09-03 15:34:56 -0700

    Change 3D asset

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-200">
-      <a rel="ar" href="/Mesh_container.usdz">
+      <a rel="ar" href="/Praying.usdz">
         <Image
           src="/thumbnail.png"
           width={500}
