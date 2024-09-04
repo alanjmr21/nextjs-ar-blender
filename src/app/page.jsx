f650a8ad5671d4096708c89b1a794c1fc70223ad
Alan Muñoz <alanjmr206@gmail.com>
2024-09-03 21:08:00 -0700

    add .gltf file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-200">
-      <a href="intent://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/alanjmr21/nextjs-ar-blender/android-ar/public/silverbullet.glb;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;">Avocado</a>
+      <a href="intent://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/alanjmr21/nextjs-ar-blender/android-ar/public/Mesh_container.gltf;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;">Avocado</a>
     </div>
   );
 }
